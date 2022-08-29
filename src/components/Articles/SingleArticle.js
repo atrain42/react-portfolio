e4ccc73cd210b1061a0a6223403f6182633e2306
atrain42 <maxheimer4211@gmail.com>
2022-08-29 11:56:59 -0400

    updates

--- src/components/Articles/SingleArticle.js
+++ src/components/Articles/SingleArticle.js
@@ -1,24 +1,35 @@
 import React from "react";
-
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import { Link } from "react-router-dom";
 import classes from "./SingleArticle.module.css";
 
 import ReadMore from "../ReadMore/ReadMore";
 
 const SingleArticle = ({ data }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   const slicedData = data.slice(0, 2);
   return (
-    <React.Fragment>
-      {slicedData.map((card, index) => (
-        <div className={classes.singleArticle} key={index}>
-          <h1>{card.title}</h1>
-          <p>{card.description}</p>
-          <Link to={`/article/${card.title}`}>
-            <ReadMore />
-          </Link>
-        </div>
-      ))}
-    </React.Fragment>
+    <section
+      ref={ref}
+      style={{
+        opacity: isInView ? 1 : 0,
+        transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1)",
+      }}
+    >
+      <React.Fragment>
+        {slicedData.map((card, index) => (
+          <div className={classes.singleArticle} key={index}>
+            <h1>{card.title}</h1>
+            <p>{card.description}</p>
+            <Link to={`/article/${card.title}`}>
+              <ReadMore />
+            </Link>
+          </div>
+        ))}
+      </React.Fragment>
+    </section>
   );
 };
 
